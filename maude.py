54f7336b106ffcbaed1ebec6fadd725b454cbd40
andrewjlawrence <ajlawrence@acm.org>
2016-05-07 23:17:03 +0100

    Added attribute, condition fragment and sort

--- maude.py
+++ maude.py
@@ -1,3 +1,4 @@
+# Copyright 2016 Andrew Lawrence
 import pyparsing as pp
 
 # Label identifier. Simple identifier
@@ -60,14 +61,51 @@ directory = pp.Word(pp.alphanums)
 lsflags = pp.Word(pp.alphanums)
 
 # Hook.
-hook = ("id-hook" + token + pp.Optional(pp.Group("(" + tokenstring + ")"))) | ((pp.Literal("op-hook") | pp.Literal("term-hook")) + pp.Group("(" + tokenstring + ")"))
+hook = ("id-hook" + token + pp.Optional(pp.Group("(" + tokenstring + ")"))) | \
+       ((pp.Literal("op-hook") | pp.Literal("term-hook")) + pp.Group("(" + tokenstring + ")"))
 
 # Print Item.
 printitem = stringid | varid | varandsortid
 
 # Statement Attribute
-statementattr = "[" + pp.OneOrMore(pp.Literal("nonexec") | pp.Literal("otherwise") | pp.Literal("variant") | pp.Group(pp.Literal("metadata") + stringid) | pp.Group(pp.Literal("label") + labelid) | pp.Group(pp.Literal("print") + pp.ZeroOrMore(printitem))) + "]"
+statementattr = "[" + pp.OneOrMore(pp.Literal("nonexec") |
+                                   pp.Literal("otherwise") |
+                                   pp.Literal("variant") |
+                                   pp.Group(pp.Literal("metadata") + stringid) |
+                                   pp.Group(pp.Literal("label") + labelid) |
+                                   pp.Group(pp.Literal("print") + pp.ZeroOrMore(printitem))) + "]"
 
+# Attribute
+attr = "[" + pp.OneOrMore(pp.Literal("assoc") |
+                               pp.Literal("comm") |
+                               pp.Group(pp.Optional(pp.Literal("left") | pp.Literal("right")) + "id:" + term )|
+                               pp.Literal("idem") |
+                               pp.Literal("iter") |
+                               pp.Literal("memo") |
+                               pp.Literal("ditto") |
+                               pp.Literal("config") |
+                               pp.Literal("obj") |
+                               pp.Literal("msg") |
+                               pp.Group(pp.Literal("metadata") + stringid) |
+                               pp.Group(pp.Literal("strat") + "(" + pp.OneOrMore(nat) + ")") |
+                               pp.Group(pp.Literal("poly")  + "(" + pp.OneOrMore(nat) + ")") |
+                               pp.Literal("frozen") + pp.Optional("(" + pp.OneOrMore(nat) + ")") |
+                               pp.Literal("prec") + nat |
+                               pp.Literal("gather") + "(" +
+                                    (pp.OneOrMore(pp.Literal("e") | pp.Literal("E") | pp.Literal("&"))) + ")" |
+                               pp.Literal("format") + "(" + pp.OneOrMore(token) + ")" |
+                               pp.Literal("special") + "(" + pp.OneOrMore(hook) + ")") + "]"
 
-test = "[print \"meh\" \"meh\" \"meh\"]"
-print (test, "->", statementattr.parseString(test))
+# Sort
+sort = pp.Forward()
+sort << sortid | sort + "{" + sort + pp.ZeroOrMore("," + sort) + "}"
+
+# Condition fragment
+conditionfragment = pp.Forward()
+conditionfragmentprime = conditionfragment | term + "=>" + term
+conditionfragment << term + "=" + term | \
+                     term + ":=" + term | \
+                     term + ":" + sort
+
+test = "[comm ditto frozen(1)]"
+print (test, "->", attr.parseString(test))
