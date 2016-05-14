536b932f9c25b78af952f04095a08972a171ffec
andrewjlawrence <ajlawrence@acm.org>
2016-05-14 14:08:12 +0100

    Work in progress

--- interpreter.py
+++ interpreter.py
@@ -3,12 +3,20 @@ import ast,parser, pyparsing, sys
 
 def eval(x):
     case = {
-        ast.QuitCommand : sys.exit(0)
+        ast.QuitCommand: sys.exit(0),
+        ast.CdCommand(dir): print("Unimplemented"),
+        ast.EofCommand: print("Unimplemented"),
+        ast.InCommand: print("Unimplemented"),
+        ast.LoadCommand: print("Unimplemented"),
+        ast.LsCommand: print("Unimplemented"),
+        ast.PopDCommand : print("Unimplemented"),
+        ast.PushCommand: print("Unimplemented"),
+        ast.PwdCommand: print("Unimplemented")
     }
-    return case(x)
+    return case[type(x)]
 
 while (True):
     try:
-        eval(parser.systemcommand.parseString(input(":-"))[0])
+        eval(parser.systemcommand.parseString(input("Maude> "))[0])
     except pyparsing.ParseException:
         print("Unknown command")
