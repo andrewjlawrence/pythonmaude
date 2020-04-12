8c2641e8fa93055247103e2d6c0cefa4d755a185
andrewlawrence <andrew.lawrence@siemens.com>
2020-04-12 20:12:27 +0100

    moved file

---
+++ lib/pytermrewriting/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ermrewriting',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='andrewlawrence',
+    author_email='andrew.lawrence@siemens.com',
+    description=''
+)
